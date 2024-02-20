98d8bad2e694ac9b36c4eda867d3329d06eda67c
noorulmobin <noorulmobin0011@gmail.com>
2024-02-20 13:48:22 +0500

    new

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(({
     credientials:true 
 }))
  app.use(express.json({limit:"16kb"}))
-
-
+    app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("Public"))
+app.use(cookieParser())
 export {app}
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,21 @@
+const asynchandler =(requestHandler)=>{
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=>next(err));
+}
+
+}
+
+
+export {asynchandler}
+
+
+// const asyncHandler =(fn)=> async(req,res,next)=>{
+//     try {
+//         await fn(req,res,next);
+//     } catch (error) {
+//         res.status(error.code || 5000).json({
+//             success:false,
+//             message:error.message
+//     })
+// }
